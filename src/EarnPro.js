2ddf91570ef6be8f821db439bcf7d80f3b10150b
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-01-30 23:59:39 +0100

    unique

--- src/EarnPro.js
+++ src/EarnPro.js
@@ -3,18 +3,18 @@ import img from "./images/img-04.png";
 
 const EarnPro = () => {
   return (
-    <div className="earn">
-      <div className="earn-left">
+    <div className="section">
+      <div className="section-left">
         <h1 className="unique">
           Earn money promoting quality digital solutions
         </h1>
         <ul>
-          <li>
+          <li className="list">
             Leaverage the power of sales automation to generate leads and
             convert them to active customers and get your wallet funded with
             profits while you are practically away.
           </li>
-          <li>
+          <li className="list">
             Effortlessly build active and dynamic sales team across the globe to
             activate the power corporate sales and daily profit sharing
           </li>
