3463ef636cc8e1c91e88f35ae671c66c1b57e7ff
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-01-31 23:46:54 +0100

    petal

--- src/Petals.js
+++ src/Petals.js
@@ -5,7 +5,7 @@ import img2 from "./images/img-24.png";
 
 const Petals = () => {
   return (
-    <div>
+    <div className="petals">
       <div>
         <h1>
           Become a Livepetal Affiliate and take control of unlimited casflow
@@ -16,10 +16,33 @@ const Petals = () => {
           ease and keep huge profit flowing in constantly
         </p>
       </div>
-      <div>
-        <img src={img0} alt="" />
-        <img src={img1} alt="" />
-        <img src={img2} alt="" />
+      <div className="petals-down">
+        <div className="first">
+          <img src={img0} alt="" className="petal-img" />
+          <h1>Extremely Simplified</h1>
+          <p>
+            You don't need to be a professional, Livepetal gives you all the
+            guide you need to start building your online business empire right
+            away
+          </p>
+        </div>
+        <div className="second">
+          <img src={img1} alt="" className="petal-img" />
+          <h1>Control your Options</h1>
+          <p>
+            You can take full control of your options to determine where you
+            want your main cashflow to come from and how you want to manage them
+          </p>
+        </div>
+        <div className="third">
+          <img src={img2} alt="" className="petal-img" />
+          <h2>Active automation</h2>
+          <p>
+            By automation your online marketing campeign you are able to
+            generate massive transactions even while you are not actively
+            working
+          </p>
+        </div>
       </div>
     </div>
   );
