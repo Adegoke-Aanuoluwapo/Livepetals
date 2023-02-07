e316b7fd265083c981b2b5d4aab0c140855a5f8e
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-02-07 12:23:53 +0100

    upgrading

--- src/Navbars/Navbar.js
+++ src/Navbars/Navbar.js
@@ -8,46 +8,111 @@ import { links } from "./data";
 const Navbar = () => {
   // const { openSidebar } = useGlobalContext();
   return (
-    <nav>
-      <div className="nav-center">
-        <div className="nav-header">
-          <img
-            src="../images/petalfav.jpg"
-            className="nav-logo"
-            alt="Livepetals"
-          />
-          <h1 className="petals">Livepetals</h1>
-          <ul className="nav-links">
-            <li>
-              <a className="link-btn" as={Link} to={"/localhost"}>
-                Home
+    <div className="wsmenucontainer" style={{ minWidth: "110px" }}>
+      <div id="page" className="page">
+        <header id="header" className="header tra-menu navbar-light">
+          <div className="header-wrapper">
+            {/* <!-- Mobile Header -- */}
+            <div className="wsmobileheader clearfix">
+              <span className="smllogo">
+                <img src="../images/logo-white.png" alt="mobile-log"></img>
+              </span>
+              <a id="wsnavtoggle" className="wsanimated-arrow">
+                <span></span>
               </a>
-            </li>
-            <li>
-              <a className="link-btn" as={Link} to={"/Affiliate"}>
-                Affiliate
-              </a>
-            </li>
-            <li>
-              <a className="link-btn" href="www.livepetal.com">
-                Creators
-              </a>
-            </li>
-            <li>
-              <a className="link-btn" href="www.livepetal.com">
-                FAQS
-              </a>
-            </li>
-            <li>
-              <a className="link-btn" href="www.livepetal.com">
-                Login
-              </a>
-            </li>
-            <a className="nav-btn">Get Started</a>
-          </ul>
-        </div>
+            </div>
+            {/* NAVIGATION MENU */}
+            <div className="wsmainfull menu clearfix">
+              <div className="wsmainwp clearfix">
+                {/* HEADER LOGO */}
+                <div className="desktoplogo">
+                  <a href="#hero-4" className="logo-black">
+                    <img src="../images/logo-black.png" alt="header-logo" />
+                  </a>
+                </div>
+                <div className="desktoplogo">
+                  <a href="#hero-4" className="logo-white">
+                    <img src="images/logo-white.png" alt="header-logo" />
+                  </a>
+                </div>
+                {/* MAIN MENU */}
+                <nav className="wsmenu clearfix" style={{ height: "551px" }}>
+                  <div className="overlapblackbg"></div>
+                  <ul className="wsmenu-list nav-pink-hover">
+                    {/* MEGAMENU */}
+                    <li>
+                      <a href=".">Home</a>
+                    </li>
+                    <li>
+                      <a href="">Affiliate</a>
+                    </li>
+                    <li>
+                      <a href="creators.php">Creators</a>
+                    </li>
+                    <li>
+                      <a href="accelerated-program.php">Program</a>
+                    </li>
+                    <li>
+                      <a href="Login.php">Log In</a>
+                    </li>
+                    {/* HEADER BUTTON */}
+                    <li className="nl-simple" aria-haspopup="true">
+                      <a
+                        href="signup.php"
+                        class="btn btn-tra-white pink-hover last-link"
+                      >
+                        Get Started
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </header>
       </div>
-    </nav>
+    </div>
+
+    // <nav>
+    //   <div className="nav-center">
+    //     <div className="nav-header">
+    //       <img
+    //         src="../images/petalfav.jpg"
+    //         className="nav-logo"
+    //         alt="Livepetals"
+    //       />
+    //       <h1 className="petals">Livepetals</h1>
+    //       <ul className="nav-links">
+    //         <li>
+    //           <a className="link-btn" as={Link} to={"/localhost"}>
+    //             Home
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a className="link-btn" as={Link} to={"/Affiliate"}>
+    //             Affiliate
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a className="link-btn" href="www.livepetal.com">
+    //             Creators
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a className="link-btn" href="www.livepetal.com">
+    //             FAQS
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a className="link-btn" href="www.livepetal.com">
+    //             Login
+    //           </a>
+    //         </li>
+    //         <a className="nav-btn">Get Started</a>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
   );
 };
 // const Navbar = () => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./css/menu.css";
+import "./css/bootstrap.min.css";
 import App from "./App";
 import Navbar from "./Navbars/Navbar";
 import { AppProvider } from "./Homes/context";
