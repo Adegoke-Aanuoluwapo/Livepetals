c48237611e8ea7b39cc0f6b7f9aaa819e7e04c34
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-02-12 00:28:27 +0100

    last

--- src/Affiliates/Questions.js
+++ src/Affiliates/Questions.js
@@ -41,26 +41,33 @@ const Questions = () => {
                   style={{ visibility: "visible", animationName: "fadeInUp" }}
                 >
                   <h5 className="h5-md">
-                    How can i get started as a Livepetal Affiliate?
+                    What are the requirements for joining Livepetal Affiliate?
                   </h5>
                   <p className="p-lg">
-                    Simply locate and click on the signup button and provide the
-                    required information to signup. The process takes less than
-                    5 minutes.{" "}
+                    No basic requirement and no skills required. You only need
+                    to be willing and ready to learn new digital skills that you
+                    can apply to selling online.
                   </p>
                 </div>
                 <div
                   className="question wow fadeInUp"
                   style={{ visibility: "visible", animationName: "fadeInUp" }}
                 >
-                  <h5 className="h5-md">
-                    How can i get started as a Livepetal Affiliate?
-                  </h5>
-                  <p className="p-lg">
-                    Simply locate and click on the signup button and provide the
-                    required information to signup. The process takes less than
-                    5 minutes.{" "}
-                  </p>
+                  <h5 className="h5-md">Who can join Livepetal Affiliate?</h5>
+                  <ul className="simple-list">
+                    <li className="list-item">
+                      <p className="p-lg">
+                        Anyone up to the age of 12 and above can become a
+                        Livepetal Affiliate.
+                      </p>
+                    </li>
+                    <li className="list-item">
+                      <p className="p-lg">
+                        There is no country restriction. Anyone from any country
+                        around the globe can be a Livepetal Affiliate.
+                      </p>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
